72280810cde49f83922a5d064e451d166d2b278a
shibi18 <coffindance18@gmail.com>
2021-10-04 15:13:04 +0530

    added games

---
+++ org_rockpapercissor/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org_rockpapercissor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            choice aichoice = GetAIChoice();
+            choice userchoice = GetHUMchoice();
+            choosewinner (aichoice, userchoice);
+            Console.ReadLine();
+        }
+        //////////////////////////////////////////////       
+
+
+        #region getaichoice
+        public static choice GetAIChoice()
+        {
+            Random r = new Random();
+            choice aichoice = (choice)r.Next(1, 4);
+            return aichoice;
+        }
+        #endregion
+
+
+        #region gethumchoice
+        public static choice GetHUMchoice()
+        {
+
+            Console.WriteLine(" ROCK PAPER SCISSOR ");
+            Console.WriteLine(" enter 1 for rock || enter 2 for paper || enter 3 for scissor");
+            Console.WriteLine(" enter your choice of weapon", 1, 3);
+            int humchoice = Convert.ToInt32(Console.ReadLine());
+            return (choice)humchoice;
+
+
+        }
+        #endregion
+
+
+        #region choosewinner
+        public static void choosewinner(choice botchoice, choice humchoice)
+        {
+            if (botchoice == choice.rock)
+            {
+                if (humchoice == choice.rock)
+                {
+                    Console.WriteLine("opponent cho0se rock");
+                    Console.WriteLine("mathc ties : !");
+                }
+                else if (humchoice == choice.paper)
+                {
+                    Console.WriteLine("opponent cho0se rock");
+                    Console.WriteLine("hurray!!");
+                    Console.WriteLine("you have won : )");
+                }
+                else if (humchoice == choice.scissors)
+                {
+                    Console.WriteLine("opponent cho0se rock");
+                    Console.WriteLine("o0ps");
+                    Console.WriteLine("you lost ; (");
+                }
+                else
+                {
+                    Console.WriteLine("choose 1,2,3");
+                }
+            }
+            if (botchoice == choice.paper)
+            {
+                if (humchoice == choice.rock)
+                {
+                    Console.WriteLine("opponent cho0se paper");
+                    Console.WriteLine("o0ps");
+                    Console.WriteLine("you lost ; (");
+                }
+                else if (humchoice == choice.paper)
+                {
+                    Console.WriteLine("opponent cho0se paper");
+                    Console.WriteLine("match ties : !");
+                }
+                else if (humchoice == choice.scissors)
+                {
+                    Console.WriteLine("opponent cho0se paper");
+                    Console.WriteLine("hurray!!");
+                    Console.WriteLine("you have won : )");
+                }
+                else
+                {
+
+                    Console.WriteLine("1,2,3");
+                }
+            }
+            if (botchoice == choice.scissors)
+            {
+                if (humchoice == choice.rock)
+                {
+                    Console.WriteLine("opponent cho0se scissor");
+                    Console.WriteLine("hurray!!");
+                    Console.WriteLine("you have won : )");
+                }
+                else if (humchoice == choice.paper)
+                {
+                    Console.WriteLine("opponent cho0se scissor");
+                    Console.WriteLine("o0ps");
+                    Console.WriteLine("you lost ; )");
+                }
+                else if (humchoice == choice.scissors)
+                {
+                    Console.WriteLine("opponent cho0se scissor");
+                    Console.WriteLine("match ties : !");
+                }
+                else
+                {
+                    Console.WriteLine("1,2,3");
+                }
+            }
+        }
+        #endregion
+      
+
+    }
+     public enum choice
+        {
+            rock   =1,
+           paper   =2,
+         scissors  =3
+        }
+}
---
+++ rockpaperscissor/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockpaperscissor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // BOTCHOICE
+            // 1 = rock
+            // 2 = paper
+            // 3 = scissor
+            Console.WriteLine("enter your choice of weapon");
+            string humchoice = (Console.ReadLine());
+
+            Random r = new Random();
+            int botchoice = r.Next(4);
+            if (botchoice == 1)
+            {
+                if(humchoice == "rock") 
+                {
+                    Console.WriteLine("opponent cho0se rock");
+                    Console.WriteLine("mathc ties : !");
+                }
+                else if (humchoice == "paper")
+                {
+                    Console.WriteLine("opponent cho0se rock");
+                    Console.WriteLine("hurray!!");
+                    Console.WriteLine("you have won : )");
+                }
+                else if (humchoice == "scissor")
+                {
+                    Console.WriteLine("opponent cho0se rock");
+                    Console.WriteLine("o0ps");
+                    Console.WriteLine("you lost ; (");
+                }
+                else
+                {
+                    Console.WriteLine("choose rock, paper or scissor");
+                }
+            }
+            if (botchoice == 2)
+            {
+                if (humchoice == "rock")
+                {
+                    Console.WriteLine("opponent cho0se paper");
+                    Console.WriteLine("o0ps");
+                    Console.WriteLine("you lost ; (");
+                }
+                else if (humchoice == "paper")
+                {
+                    Console.WriteLine("opponent cho0se paper");
+                    Console.WriteLine("match ties : !");
+                }
+                else if (humchoice == "scissor")
+                {
+                    Console.WriteLine("opponent cho0se paper");
+                    Console.WriteLine("hurray!!");
+                    Console.WriteLine("you have won : )");
+                }
+                else
+                {
+
+                    Console.WriteLine("choose rock, paper or scissor");
+                }
+            }
+            if (botchoice == 3)
+            {
+                if (humchoice == "rock")
+                {
+                    Console.WriteLine("opponent cho0se scissor");
+                    Console.WriteLine("hurray!!");
+                    Console.WriteLine("you have won : )");
+                }
+                else if (humchoice == "paper")
+                {
+                    Console.WriteLine("opponent cho0se scissor");
+                    Console.WriteLine("o0ps");
+                    Console.WriteLine("you lost ; )");
+                }
+                else if (humchoice == "scissor")
+                {
+                    Console.WriteLine("opponent cho0se scissor");
+                    Console.WriteLine("match ties : !");
+                }
+                else
+                {
+                    Console.WriteLine("choose rock, paper or scissor");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
